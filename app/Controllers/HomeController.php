593a60f9b9dab1e82ea85d7b2bceef825199301e
Ahmad Hasyim <lordhasyim@gmail.com>
2017-12-18 14:32:28 +0700

    add eloquent and signup feature

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,12 +3,14 @@
 namespace App\Controllers;
 
 use Slim\Views\Twig as View;
-
+use App\Models\User;
 class HomeController extends Controller
 {
 
     public function index($request, $response)
     {
+
+        
         // view passes to the Controller class
         return $this->view->render($response, 'home.twig');
     }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,10 +6,31 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = new \Slim\App([
     'settings' => [
         'displayErrorDetails' => true,
+    
+    'db' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'slim-fit',
+        'username' => 'root',
+        'password' => 'mysql',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => '',
     ]
+],
 ]);
 
 $container = $app->getContainer();
+
+
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function ($container) use ($capsule) {
+    return $capsule;
+};
 
 $container['view'] = function ($container) {
     $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
@@ -28,4 +49,8 @@ $container['HomeController'] = function ($container) {
     return new \App\Controllers\HomeController($container);
 };
 
-require __DIR . '/../app/routes.php';
+$container['AuthController'] = function ($container) {
+    return new \App\Controllers\Auth\AuthController($container);
+};
+
+require __DIR__ . '/../app/routes.php';
